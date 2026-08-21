9a0e7b36efa677eb430d815c8c996b65bd73b96d
Mojimini <icesattavut@gmail.com>
2026-08-21 11:48:16 +0700

    feat: export StarRailScore adapter

---
+++ src/analysis/sources/starrail-score/index.ts
@@ -0,0 +1,11 @@
+export {
+  STAR_RAIL_SCORE_DATASET_NAME,
+  StarRailScoreAdapter,
+  StarRailScoreAdapterError,
+  parseStarRailScoreCharacter,
+} from "./adapter";
+export type {
+  StarRailScoreAdapterErrorCode,
+  StarRailScoreDatasetResponse,
+  StarRailScoreTransport,
+} from "./adapter";
