85d3d1de00061794144a11c01f62015c4409b8df
Mojimini <icesattavut@gmail.com>
2026-08-21 11:49:38 +0700

    feat: export build analysis presentation API

---
+++ src/features/build-analysis/index.ts
@@ -0,0 +1,25 @@
+export {
+  AnalysisOutcome,
+  BuildAnalysisPanel,
+  CommunityRelicScores,
+  ObservedBuildEvidence,
+} from "./BuildAnalysisPanel";
+
+export type {
+  AnalysisOutcomeProps,
+  BuildAnalysisPanelProps,
+  CommunityRelicScoresProps,
+  EvidenceStatusProps,
+  ObservedBuildEvidenceProps,
+  ProvenanceListProps,
+} from "./BuildAnalysisPanel";
+
+export {
+  evidenceStateDescription,
+  evidenceStateLabel,
+  formatObservedValue,
+  formatTargetRange,
+  relicMethodLabel,
+  sourceIdentity,
+  sourceKindLabel,
+} from "./helpers";
